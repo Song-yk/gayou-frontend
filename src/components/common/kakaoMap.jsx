6a9d53fd313d06447c8f17ab67a7c727fbbb8037
Cho_c <ghktmsgkrh@naver.com>
2024-09-04 16:11:20 +0900

    feat: real data

--- src/components/common/kakaoMap.jsx
+++ src/components/common/kakaoMap.jsx
@@ -1,48 +1,66 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useCallback } from 'react';
 import { Controller } from 'react-hook-form';
+import AddIcon from '@mui/icons-material/Add';
+import RemoveIcon from '@mui/icons-material/Remove';
 
 function KakaoMap(props) {
   const { kakao } = window;
   const { width, height, name, control, center } = props;
   const mapRef = useRef(null);
+  const mapInstance = useRef(null);
 
   useEffect(() => {
-    if (kakao && kakao.maps) {
+    if (kakao && kakao.maps && center && center.length > 0) {
       const container = mapRef.current;
+
+      if (!container) {
+        console.error('Map container is not defined.');
+        return;
+      }
+
       const options = {
-        center: new kakao.maps.LatLng(36.3504119, 127.3845475),
+        center: new kakao.maps.LatLng(center[0].mapy, center[0].mapx),
         level: 2,
       };
-      const map = new kakao.maps.Map(container, options);
-      var bounds = new kakao.maps.LatLngBounds();
 
+      const map = new kakao.maps.Map(container, options);
+      mapInstance.current = map;
+
+      const bounds = new kakao.maps.LatLngBounds();
       const waypoints = [];
+
       center.forEach((location, index) => {
-        var imageSrc =
-            'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_number_blue.png',
-          imageSize = new kakao.maps.Size(36, 37),
-          imgOptions = {
-            spriteSize: new kakao.maps.Size(36, 691),
-            spriteOrigin: new kakao.maps.Point(0, index * 46 + 10),
-            offset: new kakao.maps.Point(13, 37),
-          },
-          markerImage = new kakao.maps.MarkerImage(
-            imageSrc,
-            imageSize,
-            imgOptions
-          );
+        const imageSrc =
+          'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_number_blue.png';
+        const imageSize = new kakao.maps.Size(36, 37);
+        const imgOptions = {
+          spriteSize: new kakao.maps.Size(36, 691),
+          spriteOrigin: new kakao.maps.Point(0, index * 46 + 10),
+          offset: new kakao.maps.Point(13, 37),
+        };
+
+        const markerImage = new kakao.maps.MarkerImage(
+          imageSrc,
+          imageSize,
+          imgOptions
+        );
+        const markerPosition = new kakao.maps.LatLng(
+          location.mapy,
+          location.mapx
+        );
         const marker = new kakao.maps.Marker({
-          position: new kakao.maps.LatLng(location.mapx, location.mapy),
+          position: markerPosition,
           image: markerImage,
         });
-        marker.setMap(map);
-        bounds.extend(new kakao.maps.LatLng(location.mapx, location.mapy));
 
-        if (index != 0 && index != center.length - 1) {
+        marker.setMap(map);
+        bounds.extend(markerPosition);
+
+        if (index !== 0 && index !== center.length - 1) {
           waypoints.push({
             name: `name${index}`,
-            x: location.mapy,
-            y: location.mapx,
+            x: location.mapx,
+            y: location.mapy,
           });
         }
       });
@@ -53,10 +71,10 @@ function KakaoMap(props) {
         .VITE_KAKAO_MAP_REST_API_KEY;
       const url = 'https://apis-navi.kakaomobility.com/v1/waypoints/directions';
 
-      const origin = { x: center[0].mapy, y: center[0].mapx };
+      const origin = { x: center[0].mapx, y: center[0].mapy };
       const destination = {
-        x: center[center.length - 1].mapy,
-        y: center[center.length - 1].mapx,
+        x: center[center.length - 1].mapx,
+        y: center[center.length - 1].mapy,
       };
 
       const headers = {
@@ -80,16 +98,17 @@ function KakaoMap(props) {
 
           const resData = await response.json();
 
-          console.log(resData);
+          if (!resData.routes || resData.routes.length === 0) {
+            console.error('No routes found in response:', resData);
+            return;
+          }
 
           const linePath = [];
           resData.routes[0].sections.forEach(section => {
             section.roads.forEach(router => {
               if (router.vertexes && router.vertexes.length > 0) {
                 for (let i = 0; i < router.vertexes.length; i += 2) {
-                  // Ensure we don't go out of bounds
                   if (i + 1 < router.vertexes.length) {
-                    // Add the coordinates to the path
                     linePath.push(
                       new kakao.maps.LatLng(
                         router.vertexes[i + 1],
@@ -107,38 +126,98 @@ function KakaoMap(props) {
             });
           });
 
-          var polyline = new kakao.maps.Polyline({
+          const polyline = new kakao.maps.Polyline({
             path: linePath,
             strokeWeight: 3,
             strokeColor: 'red',
             strokeOpacity: 1,
             strokeStyle: 'solid',
           });
-          polyline.setMap(map);
+
+          try {
+            polyline.setMap(map);
+          } catch (error) {
+            console.error('Error setting polyline on map:', error);
+          }
         } catch (error) {
-          console.error('Error:', error);
+          console.error('Error fetching route data:', error);
         }
       };
 
       fetchData();
+    } else {
+      console.warn(
+        'Kakao or Kakao Maps is not loaded or center is not defined.'
+      );
     }
-  }, [center]);
+  }, [kakao, center]);
+
+  const setMapType = useCallback(maptype => {
+    if (mapInstance.current) {
+      const map = mapInstance.current;
+      const roadmapControl = document.getElementById('btnRoadmap');
+      const skyviewControl = document.getElementById('btnSkyview');
+      if (maptype === 'roadmap') {
+        map.setMapTypeId(kakao.maps.MapTypeId.ROADMAP);
+        roadmapControl.className = 'selected_btn';
+        skyviewControl.className = 'custom_type_btn';
+      } else {
+        map.setMapTypeId(kakao.maps.MapTypeId.HYBRID);
+        skyviewControl.className = 'selected_btn';
+        roadmapControl.className = 'custom_type_btn';
+      }
+    }
+  }, []);
+
+  const zoomIn = useCallback(() => {
+    if (mapInstance.current) {
+      const map = mapInstance.current;
+      map.setLevel(map.getLevel() - 1);
+    }
+  }, []);
+
+  const zoomOut = useCallback(() => {
+    if (mapInstance.current) {
+      const map = mapInstance.current;
+      map.setLevel(map.getLevel() + 1);
+    }
+  }, []);
 
   return (
     <Controller
       name={name}
       control={control}
-      render={({
-        field: { onChange, value },
-        fieldState: { error },
-        formState,
-      }) => (
-        <div>
+      render={({ field: { onChange, value }, fieldState: { error } }) => (
+        <div className="map_wrap">
           <div
             ref={mapRef}
             id="map"
             style={{ width: width || '100%', height: height || '582px' }}
           ></div>
+          <div className="custom_typecontrol radius_border">
+            <span
+              id="btnRoadmap"
+              className="selected_btn"
+              onClick={() => setMapType('roadmap')}
+            >
+              지도
+            </span>
+            <span
+              id="btnSkyview"
+              className="custom_type_btn"
+              onClick={() => setMapType('skyview')}
+            >
+              스카이뷰
+            </span>
+          </div>
+          <div className="custom_zoomcontrol radius_border">
+            <span onClick={zoomIn}>
+              <AddIcon alt="확대" />
+            </span>
+            <span onClick={zoomOut}>
+              <RemoveIcon alt="축소" />
+            </span>
+          </div>
           {error && <span>{error.message}</span>}
         </div>
       )}
--- src/pages/routepage/routeCreator.jsx
+++ src/pages/routepage/routeCreator.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import axios from 'axios';
 import { useForm } from 'react-hook-form';
 import KakaoMap from '../../components/common/kakaoMap.jsx';
@@ -11,19 +11,24 @@ import { Box } from '@mui/material';
 const RouteCreator = () => {
   const [myData, setMyData] = useState([]);
   const [loading, setLoading] = useState(true);
-  const [mapHeight, setMapHeight] = useState('500px');
   const [editMode, setEditMode] = useState(false);
+  const [mapCenter, setMapCenter] = useState(null);
+  const [selectedDay, setSelectedDay] = useState('');
 
   const { control } = useForm();
 
   const GetData = async () => {
-    const response = await axios.get('/api/route/locations');
-    setMyData(response.data);
-    setLoading(false);
-
-    if (response.data.length > 0) {
-      const calculatedHeight = `${response.data.length * 116.4}px`;
-      setMapHeight(calculatedHeight);
+    try {
+      const response = await axios.get('/api/flask/route/locations/');
+      setMyData(response.data);
+      if (response.data.data && Object.values(response.data.data).length > 0) {
+        setMapCenter(response.data.data['day1']);
+        setSelectedDay('day1');
+      }
+    } catch (error) {
+      console.error('Failed to fetch data:', error);
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -35,30 +40,109 @@ const RouteCreator = () => {
     transition: 'all 0.2s ease',
   }));
 
-  const moveCardUp = idx => {
-    if (idx > 0) {
-      const newItems = [...myData.items];
-      [newItems[idx - 1], newItems[idx]] = [newItems[idx], newItems[idx - 1]];
-      setMyData(prevData => ({ ...prevData, items: newItems }));
-    }
-  };
+  const moveCardUp = useCallback(
+    idx => {
+      if (idx > 0) {
+        const newItems = [...myData.data];
+        [newItems[idx - 1], newItems[idx]] = [newItems[idx], newItems[idx - 1]];
+        setMyData(prevData => ({ ...prevData, data: newItems }));
+      }
+    },
+    [myData]
+  );
 
-  const moveCardDown = idx => {
-    if (idx < myData.items.length - 1) {
-      const newItems = [...myData.items];
-      [newItems[idx], newItems[idx + 1]] = [newItems[idx + 1], newItems[idx]];
-      setMyData(prevData => ({ ...prevData, items: newItems }));
-    }
-  };
+  const moveCardDown = useCallback(
+    idx => {
+      if (idx < myData.data.length - 1) {
+        const newItems = [...myData.data];
+        [newItems[idx], newItems[idx + 1]] = [newItems[idx + 1], newItems[idx]];
+        setMyData(prevData => ({ ...prevData, data: newItems }));
+      }
+    },
+    [myData]
+  );
 
-  const removeCard = idx => {
-    const newItems = myData.items.filter((_, cardIdx) => cardIdx !== idx);
-    setMyData(prevData => ({ ...prevData, items: newItems }));
-  };
+  const removeCard = useCallback(
+    idx => {
+      const newItems = myData.data.filter((_, cardIdx) => cardIdx !== idx);
+      setMyData(prevData => ({ ...prevData, data: newItems }));
+    },
+    [myData]
+  );
 
   const toggleEditMode = () => {
     setEditMode(!editMode);
   };
+
+  function repeatRoutes(obj) {
+    return obj.map((location, index) => (
+      <MyCardControls
+        key={`location-${index}-${location.title}`}
+        width="100%"
+        image={location.firstimage}
+        alt={location.title}
+        title={location.title}
+        overview={location.overview}
+        idx={index}
+        totalItems={obj.length}
+        moveCardUp={() => moveCardUp(index)}
+        moveCardDown={() => moveCardDown(index)}
+        removeCard={() => removeCard(index)}
+        showIcons={editMode}
+      />
+    ));
+  }
+
+  function repeatRoutesSubTitle(obj) {
+    return Object.values(obj).map((day, index) => (
+      <React.Fragment key={`day-${index}`}>
+        <Box key={`day-${index}-box`}>
+          <h4 style={{ textAlign: 'left', fontWeight: 'bold' }}>
+            {index + 1}일차
+          </h4>
+        </Box>
+        <Box
+          key={`day-${index}-box2`}
+          sx={{
+            border: 'solid 1px',
+            borderRadius: '20px',
+            padding: '1em',
+            borderColor: '#a6a6a6',
+            height: '450px',
+            overflow: 'auto',
+          }}
+        >
+          {repeatRoutes(Object.values(day))}
+        </Box>
+      </React.Fragment>
+    ));
+  }
+
+  function createMap(obj) {
+    let app = [];
+    app.push(
+      <Box sx={{ marginBottom: '1em', float: 'left' }}>
+        {Object.values(obj).map((dayKey, index) => (
+          <MyButtonField
+            key={`dayButton-${index}`}
+            name="day"
+            control={control}
+            color={
+              selectedDay === `day${index + 1}`
+                ? 'sunsetOrangeSelected'
+                : 'sunsetOrange'
+            }
+            value={`${index + 1}일차`}
+            onClick={() => {
+              setMapCenter(myData.data[`day${index + 1}`]); // Dynamically set the center based on the day
+              setSelectedDay(`day${index + 1}`); // Update selected day
+            }}
+          />
+        ))}
+      </Box>
+    );
+    return app;
+  }
 
   return (
     <div className="options">
@@ -70,7 +154,7 @@ const RouteCreator = () => {
               AI 동네투어를 생성중이에유. <br />
             </h1>
           </div>
-        ) : myData.items.length > 0 ? (
+        ) : myData.day > 0 ? (
           <Box sx={{ width: '80%' }}>
             <Grid container spacing={3}>
               <AnimatedGridItem item xs={12}>
@@ -107,38 +191,7 @@ const RouteCreator = () => {
                     {myData.courseTite}
                   </h5>
                 </Box>
-                <Box>
-                  <h4 style={{ textAlign: 'left', fontWeight: 'bold' }}>
-                    1일차
-                  </h4>
-                </Box>
-                <Box
-                  sx={{
-                    border: 'solid 1px',
-                    borderRadius: '20px',
-                    padding: '1em',
-                    borderColor: '#a6a6a6',
-                    height: '450px',
-                    overflow: 'auto',
-                  }}
-                >
-                  {myData.items.map((location, index) => (
-                    <MyCardControls
-                      key={index}
-                      width="100%"
-                      image={location.firstimage}
-                      alt={location.title}
-                      title={location.title}
-                      description={location.title}
-                      idx={index}
-                      totalItems={myData.items.length}
-                      moveCardUp={() => moveCardUp(index)}
-                      moveCardDown={() => moveCardDown(index)}
-                      removeCard={() => removeCard(index)}
-                      showIcons={editMode}
-                    />
-                  ))}
-                </Box>
+                {repeatRoutesSubTitle(myData.data)}
                 <Box sx={{ textAlign: 'right' }}>
                   <MyButtonField
                     width="25%"
@@ -152,21 +205,20 @@ const RouteCreator = () => {
               </AnimatedGridItem>
               <AnimatedGridItem
                 item
+                style={{ paddingTop: 0 }}
                 xs={12}
                 sm={12}
-                md={6}
-                lg={7}
+                md={4}
+                lg={5}
                 xl={7}
                 xxl={7}
               >
-                <Box>
-                  <KakaoMap
-                    name="location"
-                    control={control}
-                    center={myData.items}
-                    height={mapHeight}
-                  />
-                </Box>
+                {createMap(myData.data)}
+                <KakaoMap
+                  name="location"
+                  control={control}
+                  center={mapCenter}
+                />
               </AnimatedGridItem>
               <AnimatedGridItem item xs={12} md={12}>
                 <Box sx={{ textAlign: 'right' }}>
